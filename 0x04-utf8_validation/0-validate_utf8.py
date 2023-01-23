2cd5d819f59b6add13ef42eaa241e07639fd0826
joshua530 <joshuaomari5@gmail.com>
2023-01-23 07:30:22 +0300

    add files for utf-8 validation task

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''
+Contains method that determines if a given data set represents a
+valid UTF-8 encoding
+'''
+
+
+def validUTF8(data):
+    '''
+    determines if a given data set represents a valid UTF-8 encoding
+    '''
