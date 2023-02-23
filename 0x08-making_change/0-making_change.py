1ceeab3accf8bee7ab7d9b28ed9d4de25b484b74
joshua530 <joshuaomari5@gmail.com>
2023-02-23 11:42:02 +0300

    update prototype

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,19 +7,19 @@ making change solution
 from typing import Dict, List
 
 
-def makeChange(coins: List[int], amount: int) -> int:
+def makeChange(coins: List[int], total: int) -> int:
     '''
     making change solution function
     '''
     # dynamic programming -> tabulation
-    dp = [amount + 1] * (amount + 1)
+    dp = [total + 1] * (total + 1)
     dp[0] = 0
 
-    for a in range(1, amount + 1):
+    for a in range(1, total + 1):
         for c in coins:
             if a - c >= 0:
                 dp[a] = min(dp[a], 1 + dp[a-c])
-    return dp[amount] if dp[amount] != amount + 1 else -1
+    return dp[total] if dp[total] != total + 1 else -1
 
     # dumb!
     # num_coins = 0
